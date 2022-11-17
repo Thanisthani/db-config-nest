ff3431ff8cb26a9d8fa6e62725b08a095178c06a
Thanistas <thanithanis171198@gmail.com>
2022-11-17 15:57:50 +0530

    upload dynamodb

---
+++ nest_dynamodb/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(
+      {
+        envFilePath: '.env',
+        isGlobal: true
+      }
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ nest_dynamodb/src/user/user.service.ts
@@ -0,0 +1,126 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+    config,
+    DynamoDB
+} from 'aws-sdk'
+import { userCreatedto } from './dto/userCreate.dto';
+import { v4 as uuid } from 'uuid';
+import { encodePassword } from 'src/utils/bcrypt';
+import { logindto } from 'src/user/dto/login.dto';
+import { compare } from 'bcrypt';
+
+
+@Injectable()
+    
+export class UserService {
+
+   private con =  config.update({
+        region: process.env.REGION,
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRECT_ACCESS_KEY_ID
+    });
+
+    private dynamoClient = new DynamoDB.DocumentClient();
+
+    private TABLE_NAME = 'nestjs';
+
+// Register user
+    
+    async register(userCreateDTO: userCreatedto) {
+        const existingUser = await this.getUserByEmail(userCreateDTO.email);
+
+        if (!existingUser)
+        {
+            const password = await encodePassword(userCreateDTO.password);
+            const user = {
+                userid: uuid(),
+                ...userCreateDTO,
+                password
+                
+            }
+    
+            try {
+
+                await this.dynamoClient
+                    .put({
+                
+                        TableName: this.TABLE_NAME,
+                        Item: user,
+              
+                    })
+                    .promise();
+            }
+            catch (error)
+            {
+                throw new HttpException({
+                    status: HttpStatus.NOT_ACCEPTABLE,
+                    message: error
+                }, HttpStatus.NOT_ACCEPTABLE);
+            }
+    
+            throw new HttpException({
+                status: HttpStatus.CREATED,
+                message: 'Sucessfully created'
+            }, HttpStatus.CREATED);
+
+        }
+
+        throw new HttpException({
+            status: HttpStatus.NOT_ACCEPTABLE,
+            message: 'Already email registered'
+        }, HttpStatus.NOT_ACCEPTABLE);
+        
+    }
+
+        // Validate user
+    
+        async validateUser(loginDTO: logindto) {
+            const user = await this.getUserByEmail(loginDTO.email);
+    
+            if (user)
+            {
+                const isMatch = await compare(loginDTO.password, user.password);
+    
+                if (isMatch)
+                {
+                    throw new HttpException({
+                        status: HttpStatus.ACCEPTED,
+                        message: 'Sucessfully Logged In',
+                        user: user
+                    }, HttpStatus.ACCEPTED);
+                }
+    
+                throw new HttpException({
+                    status: HttpStatus.UNAUTHORIZED,
+                    message: 'Incorrect password',
+                    user: null
+                }, HttpStatus.UNAUTHORIZED);
+                
+            }
+
+            throw new HttpException({
+                status: HttpStatus.NOT_ACCEPTABLE,
+                message: 'User not exists',
+                user: null
+            }, HttpStatus.NOT_ACCEPTABLE);
+    
+        }
+
+    // find user by email ID
+
+   async  getUserByEmail(email: string) {
+        const params = {
+            TableName : this.TABLE_NAME,
+            FilterExpression : 'email = :email',
+            ExpressionAttributeValues : {':email' : email}
+        };
+  
+ 
+             const resp =await this.dynamoClient.scan(params).promise(); 
+        
+       return resp.Items[0];
+    }
+
+
+
+}
