3d31c186317139c19576604881cfda0ea98e72cb
Thanistas <thanithanis171198@gmail.com>
2022-11-17 15:53:02 +0530

    upload mysql

---
+++ nest_mysql/src/user/user.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User } from 'database/modules/user.entity';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(
+      {
+        envFilePath: '.env',
+        isGlobal: true
+        }),
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn:'1d'}
+  })
+  ],
+  providers: [
+    UserService,
+    LocalStrategy,
+    JwtStrategy
+  ],
+  controllers: [UserController]
+})
+export class UserModule {}
